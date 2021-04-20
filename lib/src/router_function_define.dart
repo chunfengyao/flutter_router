e676e570e6b82d5dab2b4c48ddb36b6e91f99f97
姚春锋 <yaochunfeng@wondersgroup.com>
2021-04-20 16:33:45 +0800

    change{注释。}

--- lib/src/router_function_define.dart
+++ lib/src/router_function_define.dart
@@ -1,3 +1,10 @@
+/// <pre>
+/// Used to 路由的一些功能的定义
+/// <pre/>
+/// created by:   yaochunfeng
+/// on:           2021/4/20 4:15 下午
+/// Email:        yaochunfeng@wondersgroup.com
+
 import 'package:flutter/widgets.dart';
 
 typedef MNRouterFindPage = Widget Function(String path);
